1b53aa948e5bd14c9025541616c1dffa8c2e25bd
Ben <bmshreeve3@gmail.com>
2024-02-07 09:31:31 +0000

    animation

--- src/app/page/page.js
+++ src/app/page/page.js
@@ -3,13 +3,31 @@
 import Image from "next/image";
 import dancer1 from "@/../public/move1.png";
 import dancer2 from "@/../public/move2.png";
+import dancer3 from "@/../public/turn1.png";
+import dancer4 from "@/../public/turn2.png";
+import dancer5 from "@/../public/jump.png";
 import AnimateIn from "../component/animation";
-
 import { useState, useEffect } from "react";
+
+let i = 1;
+
 export default function page() {
+  const listOfImages = [dancer1, dancer2, dancer3, dancer4, dancer5];
   const [boolean, setBoolean] = useState(false);
+  const [spin, setSpin] = useState(false);
+
   useEffect(() => {
     const interval = setInterval(() => {
+      i++;
+      console.log(i);
+      if (i === 10 || i === 11) {
+        setSpin(true);
+      } else if (i === 12) {
+        i = -13;
+        setSpin(false);
+      } else {
+        setSpin(false);
+      }
       setBoolean((currentBoolean) => !currentBoolean);
     }, 200);
 
@@ -17,10 +35,24 @@ export default function page() {
       clearInterval(interval);
     };
   }, []);
+
+  function dancer() {
+    if (boolean) {
+      return listOfImages[0];
+    }
+    return listOfImages[1];
+  }
+
+  function spinDancer() {
+    if (i === 10 || i === 11) {
+      return listOfImages[3];
+    }
+  }
+
   return (
     <div className="animate">
       <AnimateIn>
-        <Image src={boolean ? dancer1 : dancer2} />
+        <Image src={spin ? spinDancer() : dancer()} alt="dancer" priority />
       </AnimateIn>
     </div>
   );
